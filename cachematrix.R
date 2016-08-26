213d5a73e81d619bacd892c42eeaae53844c50fd
maabari <niclas@maabari.com>
2016-08-26 11:17:52 +0200

    my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+#the following functions are used to cache the inverse of a matrix
 
-## Write a short comment describing this function
+#four functions allow to modify (set) or (get) the value of the
+#inverted matrix and to recalculate (set) and get the value of its
+#inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        #setting the mean to NULL as a placeholder for future value
+        m <- NULL
+        
+        #define a function to vector x to a new vector y and reset the mean m to NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        #return x
+        get <- function() x
+        
+        #set and return m
+        setinv <- function(solve) m <<- solve
+        getinv<- function() m
+        
+        #returns the list containing above functions defined
+        
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+#the following function returns the inverse of the matrix that was
+#created with the previous function (makeCacheMatrix). If the inverse
+#has not been calculated (is.null), or the matrix changed recently,
+#then a recalculation would be executed.
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+        m <- x$getinv()
+        if (is.null(m)) {
+                message('Computing inverse...')
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setinv(m)
+        } else {
+                message('Returning cached inverse...')
+        }
+        return(m)
 }
+
+#
+# Testing
+#
+
+# Create a cached matrix
+x <- makeCacheMatrix()
+set.seed(123)
+x$set(matrix(runif(16, -1, 1), 4))
+
+# cacheSolve() computes the inverse
+cacheSolve(x)
+
+# cacheSolve() returns the cached inverse
+cacheSolve(x)
+
+# After changing the matrix, cacheSolve() recomputes the inverse
+x$set(matrix(runif(16, -1, 1), 4))
+cacheSolve(x)
